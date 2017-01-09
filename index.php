fb145e541c0b81d600e0203ab5b80293060f51bd
Eric Frank <eric@ericfrank.com>
2017-01-08 22:05:42 -0500

    Remove Twitter icon

--- index.php
+++ index.php
@@ -42,7 +42,6 @@
 
         <footer>
             <ul>
-                <li><a class="icon-alone" href="http://www.twitter.com/ercfrnk" data-icon="&#xe000;"><div class="tooltip">Twitter</div></a></li>
                 <li><a class="icon-alone" href="mailto:eric@ericfrank.com" data-icon="&#xe001;"><div class="tooltip">Email</div></a></li>
             </ul>
         </footer>
